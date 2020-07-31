4f931a0e0dcd7e459fe11ada89226f2ea83d24e3
Nick <neverate@gmail.com>
2020-07-31 10:17:19 +0300

    stage3: laid out the class/method structure

---
+++ src/correcter/Convert.java
@@ -0,0 +1,11 @@
+package correcter;
+
+class Convert {
+    static byte[] toBinary(String msg) {
+        return new byte[5];
+    }
+
+    static String fromBinary(byte[] bytes) {
+        return "The result.";
+    }
+}
--- src/correcter/ErrorGenerator.java
+++ src/correcter/ErrorGenerator.java
@@ -1,39 +1,17 @@
 package correcter;
 
 import java.util.Random;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class ErrorGenerator {
     private static final Random RANDOM = new Random();
 
-    static String simulateErrors(String text) {
-        final int size = text.length();
-        final int errorEveryXChars = 3;
-
-        Set<Integer> indices = IntStream.iterate(0, i -> i + errorEveryXChars)
-            .limit(size / errorEveryXChars)
-            .map(i -> i + RANDOM.nextInt(errorEveryXChars))
-            .boxed().collect(Collectors.toSet());
-
-        return IntStream.range(0, text.length())
-            .mapToObj(i -> {
-                char ch = text.charAt(i);
-                if (indices.contains(i)) {
-                    ch = corruptChar(ch);
-                }
-                return String.valueOf(ch);
-            }).collect(Collectors.joining());
+    static void simulateBitLevelErrors(byte[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = corruptByte(chars[i]);
+        }
     }
 
-    static char corruptChar(char ch) {
-        char potential;
-        String legalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
-        do {
-            potential = legalChars.charAt(RANDOM.nextInt(legalChars.length()));
-        } while (potential == ch);
-
-        return potential;
+    private static byte corruptByte(byte src) {
+        return (byte) (src + 1);
     }
 }
