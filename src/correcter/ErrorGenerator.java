c5333d00a92e2fa1ca4343bfba07b7bed603a402
Nick <neverate@gmail.com>
2020-08-04 12:08:00 +0300

    stage4: send: laid out basic structure + UI output

--- src/correcter/ErrorGenerator.java
+++ src/correcter/ErrorGenerator.java
@@ -5,17 +5,7 @@ import java.util.Random;
 class ErrorGenerator {
     private static final Random RANDOM = new Random();
 
-    static void simulateBitLevelErrors(String[] chars) {
-        for (int i = 0; i < chars.length; i++) {
-            chars[i] = corruptByte(chars[i]);
-        }
-    }
-
-    private static String corruptByte(String src) {
-        StringBuilder result = new StringBuilder(src);
-        int idxToCorrupt = RANDOM.nextInt(src.length());
-        char charToCorrupt = result.charAt(idxToCorrupt);
-        result.setCharAt(idxToCorrupt, charToCorrupt == '0' ? '1' : '0');
-        return result.toString();
+    public static String corruptBytes(String bytes) {
+        return bytes;
     }
 }
