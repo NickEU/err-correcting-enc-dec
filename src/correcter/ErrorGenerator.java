d0869abb165b0090b02636e835c8f078cf2b9974
Nick <neverate@gmail.com>
2020-08-04 19:18:18 +0300

    stage4: send: +bit corruption

--- src/correcter/ErrorGenerator.java
+++ src/correcter/ErrorGenerator.java
@@ -1,11 +1,26 @@
 package correcter;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 class ErrorGenerator {
+    private static final Map<Character, Character> zeroOne = Map.of(
+        '0', '1',
+        '1', '0');
     private static final Random RANDOM = new Random();
 
     public static String corruptBytes(String bytes) {
-        return bytes;
+        return Arrays.stream(bytes.split(" "))
+            .map(ErrorGenerator::corruptByte)
+            .collect(Collectors.joining(" "));
+    }
+
+    private static String corruptByte(String original) {
+        var result = new StringBuilder(original);
+        int idxOfCorruptBit = RANDOM.nextInt(8);
+        result.setCharAt(idxOfCorruptBit, zeroOne.get(original.charAt(idxOfCorruptBit)));
+        return result.toString();
     }
 }
