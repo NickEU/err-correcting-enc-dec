50dab1c1be44284b11c590e0fabea53b96cbdb16
Nick <neverate@gmail.com>
2020-08-04 10:40:33 +0300

    stage4: +binToHex and textToBin converters

--- src/correcter/Convert.java
+++ src/correcter/Convert.java
@@ -1,11 +1,21 @@
 package correcter;
 
+import java.util.Arrays;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+
 class Convert {
     public static String textToBin(String msg) {
-        return msg;
+        return msg.chars()
+            .mapToObj(Integer::toBinaryString)
+            .map(s -> '0' + s + " ")
+            .collect(Collectors.joining());
     }
 
     public static String binToHex(String msg) {
-        return msg;
+        return Arrays.stream(msg.split(" "))
+            .mapToInt(b -> Integer.parseInt(b, 2))
+            .mapToObj(Integer::toHexString)
+            .collect(Collectors.joining(" "));
     }
 }
